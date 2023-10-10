3ad47395abe045dd7fa8ce57b345ae4fbab97c45
Mireya  Rodríguez <a01637218@tec.mx>
2023-10-10 14:16:21 +0000

    ÑAM

--- hilos.c
+++ hilos.c
@@ -3,11 +3,13 @@
 
 void* holaHilo(){
     printf("hola desde un hilo \n");
+    pthread_exit(NULL);
 }
 
 int main(){
     pthread_t t; 
     pthread_create(&t, NULL, holaHilo, NULL);
+    pthread_exit(NULL)
 
     return 0;
 }
