7d74c509a83a3285084ee54496a3606fc6a9c058
Mireya  Rodríguez <a01637218@tec.mx>
2023-10-10 14:40:04 +0000

    hilos4

--- hilos.c
+++ hilos.c
@@ -6,17 +6,25 @@ void* holaHilo(){
     pthread_exit(NULL);
 }
 
+void* incrementaSaldo(int saldo){
+    printf("el saldo actual: %d. \n", saldo);
+    saldoN = saldo + 100;
+    printf("nuevo saldo: %d. \n", saldoN);
+    pthread_exit(NULL);
+}
+
 int main(){
     pthread_t t; 
-    pthread_create(&t, NULL, holaHilo, NULL);
-    pthread_exit(NULL);
+    //pthread_create(&t, NULL, holaHilo, NULL);
+    //pthread_exit(NULL);
 
     int n = 5;
+    int saldo = 0;
 
-    for (int i : n) {
+    for (int i = 1; i <= n; i++) {
         pthread_create(&t, NULL, holaHilo, NULL);
-        pthread_exit(NULL)
+        pthread_create(&t, NULL, incrementaSaldo, NULL);
     }
-
+    pthread_exit(NULL);
     return 0;
 }
