9aad4265d6d4e0bb4efd164c9580ea7c3b7e031d
Mireya  Rodríguez <a01637218@tec.mx>
2023-10-10 14:06:30 +0000

    hilos

---
+++ hilos.c
@@ -0,0 +1,13 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void* holaHilo(){
+    printf("hola desde un hilo \n")
+}
+
+int main(){
+    pthread_t t; 
+    pthread_create(&t, NULL, holaHilo, NULL);
+
+    return 0;
+}
