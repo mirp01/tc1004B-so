ff51cbe89644334832265853c7aadddefd677218
Mireya  Rodríguez <a01637218@tec.mx>
2023-10-18 13:16:56 +0000

    hilos 2 final

--- hilos.c
+++ hilos.c
@@ -1,32 +1,28 @@
 #include <pthread.h>
 #include <stdio.h>
 
-int saldo;
 
-void* holaHilo(){
-    printf("hola desde un hilo \n");
+void* holaHilo(void *thid){
+    printf("hola desde un hilo %d\n", *((int *)thid));
     pthread_exit(NULL);
 }
 
-void* incrementaSaldo(){
-    printf("el saldo actual: %d. \n", saldo);
-    saldo = saldo + 100;
-    printf("nuevo saldo: %d. \n", saldo);
-    pthread_exit(NULL);
-}
 
 int main(){
     pthread_t t; 
-    //pthread_create(&t, NULL, holaHilo, NULL);
+
+    int n = 0;
+    int thid = 3;
+
+
+    for (int i = 1, i <= n, i++){
+        pthread_create(&t, NULL, holaHilo, &thid);
+        
+    }
+
+    //pthread_create(&t, NULL, holaHilo, &thid);
     //pthread_exit(NULL);
 
-    int n = 100;
-    int saldo = 0;
 
-    for (int i = 1; i <= n; i++) {
-        //pthread_create(&t, NULL, holaHilo, NULL);
-        pthread_create(&t, NULL, incrementaSaldo, NULL);
-    }
-    pthread_exit(NULL);
     return 0;
 }
