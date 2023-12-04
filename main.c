63df1827979659c10a8225e01744b8353d8f949c
Mireya Isabel Rodríguez Pasillas <133801089+mirp01@users.noreply.github.com>
2023-12-04 03:27:09 +0000

    fork2

--- main.c
+++ main.c
@@ -5,6 +5,15 @@
 int main(){
     fork();
     printf("hello :D!\nPID = %d\n", getpid());
+    printf("Proceso Padre\n");
+    int pid = fork();
+    if (pid==0){
+        printf("Proceso hijo\n");
+    } else {
+        printf("Proceso padre e hijo son %d, pid")
+    }
+
+    
 
     return 0;
 }
